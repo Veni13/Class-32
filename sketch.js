446f7e5fa4ddbe73d477535c2b7d59f9bb01c5c6
Veni13 <veni7678@gmail.com>
2020-04-25 17:49:02 +0530

    Made the change in background

--- sketch.js
+++ sketch.js
@@ -9,10 +9,13 @@ var backgroundImg,platform;
 var bird, slingshot;
 
 var gameState = "onSling";
+var bg = "sprites/bg.png";
+var score = 0;
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
-}
+    getBackgroundImage ();
+
+} 
 
 function setup(){
     var canvas = createCanvas(1200,400);
@@ -45,7 +48,10 @@ function setup(){
 }
 
 function draw(){
+    if(backgroundImg)
     background(backgroundImg);
+    
+   
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
@@ -85,4 +91,19 @@ function keyPressed(){
     if(keyCode === 32){
        // slingshot.attach(bird.body);
     }
+    
+}
+async function getBackgroundImage () {
+   var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
+   var responseJSON = await response.json();
+   var datetime = responseJSON.datetime;
+   var hour = datetime.slice(11,13);
+   if(hour >= 06 && hour <= 19){
+       bg = "sprites/bg.png";
+   }
+   else{
+       bg = "sprites/bg2.jpg";
+   }
+   backgroundImg = loadImage(bg);
+
 }
